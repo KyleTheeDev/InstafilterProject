ce0e7579632b6bf4494e87a2c16f6fba0bb2d3cd
KyleTheeDev <kandmiller@gmail.com>
2021-09-21 00:05:10 -0400

    Initial commit

---
+++ InstafilterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterProjectApp.swift
+//  InstafilterProject
+//
+//  Created by Kyle Miller on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
